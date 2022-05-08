d24e28331384112ccbbc986905cc09a57d6c5e11
rejaul-islam1 <rejaul.hero@gmail.com>
2022-05-08 10:41:32 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -13,6 +14,20 @@ app.use(express.json());
 
 
 
+function verifyJWToken(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 
@@ -25,7 +40,14 @@ async function run() {
         await client.connect();
         const itmeCollection = client.db('refreshResources').collection('item');
 
-
+        // Auth 
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        })
 
         // Get all Items
         app.get('/items', async (req, res) => {
@@ -56,11 +78,10 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const result = await itmeCollection.deleteOne(query);
             res.send(result);
+        })
 
-        }
-
-         // Get all Items With a Particular Email
-         app.get('/allitem', verifyJWToken, async (req, res) => {
+        // Get all Items With a Particular Email
+        app.get('/allitem', verifyJWToken, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email;
             console.log(decodedEmail)
@@ -91,10 +112,18 @@ async function run() {
             res.send(result);
         })
 
+        // Delete one Item From My Item
+        // app.delete('/items/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await itmeCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+    }
     finally {
 
-        }
     }
+}
 
 run().catch(console.dir);
 
@@ -102,10 +131,10 @@ run().catch(console.dir);
 
 
 
-    app.get('/', (req, res) => {
-        res.send('Refresh Resourses');
-    });
+app.get('/', (req, res) => {
+    res.send('Refresh Resourses');
+});
 
-    app.listen(port, () => {
-        console.log('Listening to port', port);
-    });
+app.listen(port, () => {
+    console.log('Listening to port', port);
+});
